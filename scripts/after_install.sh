8585b8ca6dea8e8a307cfda6023dabf44bcac714
Makrem-Mazroui <75634759+Makrem-Mazroui@users.noreply.github.com>
2026-01-09 17:31:45 +0100

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -5,7 +5,7 @@ sudo tee /etc/systemd/system/srv-02.service << EOL
 Description=Dotnet S3 info service
 
 [Service]
-ExecStart=/usr/bin/dotnet /home/ubuntu/srv-02/bin/Release/net8.0/linux-x64/srv02.dll
+ExecStart=/usr/bin/dotnet /home/ubuntu/srv-02/srv02.dll
 SyslogIdentifier=srv-02
 
 Environment=DOTNET_CLI_HOME=/temp
